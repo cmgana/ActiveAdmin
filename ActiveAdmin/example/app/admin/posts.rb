1fe72614ff0d99444acb96c2eecdfcbb9014ef6b
cmgana <gana.cm@rently.com>
2018-12-20 09:40:06 +0530

    comit

---
+++ ActiveAdmin/example/app/admin/posts.rb
@@ -0,0 +1,41 @@
+ActiveAdmin.register Post do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+ permit_params :title, :content, :published_at, :user_id
+
+ scope :all
+ scope :unpublished
+ scope :published
+
+ action_item :publish ,only: :show do
+  link_to "Publish" , publish_admin_post_path(post), method: :put if ! post.published_at?
+ end
+
+
+ action_item :unpublish ,only: :show do
+  link_to "Un Publish" , unpublish_admin_post_path(post), method: :put if  post.published_at?
+ end
+
+ member_action :publish, method: :put do 
+  post = Post.find(params[:id])
+  post.update(published_at: Time.zone.now)
+  redirect_to admin_post_path(post)
+end
+
+
+ member_action :unpublish, method: :put do 
+  post = Post.find(params[:id])
+  post.update(published_at: nil)
+  redirect_to admin_post_path(post)
+end
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end
